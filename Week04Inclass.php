774305f408bc23ee524a1379ef0d7703ed1758d4
Kitikawin Hompan <152171870+65160319Kitikawin@users.noreply.github.com>
2023-12-21 19:33:35 +0700

    form_101

--- Week04Inclass.php
+++ Week04Inclass.php
@@ -9,7 +9,83 @@
     <?php echo "<h1>Hello World<br><br><h1>" ?>
     <?php $var_a = "SE_CAMP" ?>
     <?php echo $var_b = 'Hello $var_a' ?>
+    <br>
     <?php echo $var_c = "Hello $var_a" ?>
+    <br>
+    <?php
+    $var_c = $var_b = 1;
+    $var_c = '1';
+    echo $var_b <=> $var_c;
+    echo "<br>";
+    if($var_b === $var_c){
+        //true
+        echo '$var_b === $var_c';
+    }else if($var_b <> $var_c){
+        //true
+        echo '$var_b <> $var_c';
+    }else {
+        //false
+        echo 'else';
+    } ?>
+    <br>
+    <?php echo $var_c = 2; ?>
+    <br>
+    <?php 
+    echo $var_c;
+    echo "<br>";
+    switch($var_c){
+        case 1:
+            echo "1";
+            break;
+        default:
+            echo "test";
+    }
+
+    $arry = array(1,2,3);
+    $arry1 = [1,2,3]; //{}
+    $arry2[] = 1;
+    $arry2[] = 2;
+    $arry2[] = 3;
+    $arry2[] = "Hello";
+    $arry2['SE'] = "World";
+    ?> 
+        <?php
+        pre($arry);
+        ?>
+
+        <?php print_r($arry);?> 
+        <pre><?php print_r($arry);?></pre>
+        <pre><?php print_r($arry1);?></pre> 
+        <pre><?php print_r($arry2);?></pre>
+
+        <?php for($i = 0; $i < count($arry2)  ; $i++){ ?>
+        <h3><?php echo $arry2[$i]; ?></h3>
+        <?php } ?>
+
+        <?php foreach($arry2 as $key => $value){ ?>
+        <h3><?php echo $arry2[$key]; ?> : <?php echo $value; ?></h3>
+        <?php } ?>
+
+        <?php foreach($arry2 as $key => $value){ ?>
+        <h3><?php echo $arry2[$key]; ?>
+        : <?php echo $key ?>
+        : <?php echo $value; ?></h3>
+        <?php } ?>
+        <?php
+            prE($arry);
+        ?>
+        <?php
+        function pre($value){
+            echo "<pre>1";
+            print_r($value);
+            echo "</pre>";
+        }
+        //function pRe($value){
+        //    echo "<pre>2";
+        //    print_r($value);
+        //    echo "</pre>";
+        //}
+        ?>
 
 </body>
 
