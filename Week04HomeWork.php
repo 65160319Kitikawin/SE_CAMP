6ff4642f11d8c6f3989d586162df8669665a6a0f
Kitikawin Hompan <152171870+65160319Kitikawin@users.noreply.github.com>
2023-12-24 22:11:59 +0700

    homeworkConfirm

--- Week04HomeWork.php
+++ Week04HomeWork.php
@@ -8,7 +8,10 @@
 <body>
     <h1>ตารางสูตรคูณตาม Input</h1>
     <form action="" method="post">
-        <input type="number" name="number" placeholder="กรอกตัวเลขแม่สูตรคูณ" />
+        <input type="number" name="number" class="number1" placeholder="ตัวเลขแม่สูตรคูณ" />
+        <br>
+        <input type="number" name="order" class="number2" placeholder="ลำดับเลขตัวคูณ" />
+        <br>
         <button type="submit">ยืนยัน</button>
     </form>
     <table>
@@ -16,8 +19,13 @@
             <?php
                 if(isset($_POST['number'])) {
                     $number = $_POST['number'];
-                    if (!empty($number)) {
-                         for($i = 1; $i < 25; $i++) {
+                    $order = $_POST['order'];
+                    if (!empty($number) && empty($order)) {
+                        for($i = 1; $i < 25; $i++) {
+                            echo "<tr><td>{$number} x {$i} = ".($number * $i)."</td></tr>";
+                        }
+                    }else if (!empty($number) && !empty($order)) {
+                        for($i = 1; $i <= $order; $i++) {
                             echo "<tr><td>{$number} x {$i} = ".($number * $i)."</td></tr>";
                         }
                     }
@@ -31,6 +39,24 @@
             height: 265px;
             overflow-y: scroll;
             display: block;
+        }
+
+        .number1::placeholder {
+            color: red;
+            font-weight: bold;
+        }
+
+        .number2::placeholder {
+            font-weight: bold;
+        }
+
+        .number2 {
+            margin-top: 5px;
+        }
+        
+        button {
+            margin-left: 61.5px;
+            margin-top: 5px;
         }
     </style>
 </body> 
