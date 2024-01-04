8b84676981e721d9c356337465277b04625fa753
Kitikawin Hompan <152171870+65160319Kitikawin@users.noreply.github.com>
2024-01-04 17:28:41 +0700

    Update myroute.blade.php

--- example-app/resources/views/myroute.blade.php
+++ example-app/resources/views/myroute.blade.php
@@ -2,11 +2,23 @@
 
     <head>
         <meta charset="utf-8" />
+        <style>
+            table {
+                width: 150px;
+                height: 265px;
+                overflow-y: scroll;
+                display: block;
+            }
+        </style>
     </head>
 
     <body>
-        <h1>My Route Page</h1>
-        <h1>{{ $myinput }}</h1>
+        <h1>สูตรคูณแม่ .$myinput</h1>
+        <table>
+            <?php  for($i = 1; $i < 25; $i++) {
+                echo "<tr><td>{$myinput} x {$i} = ".($myinput * $i)."</td></tr>";
+            } ?>
+        </table>
     </body>
 
 </html>
