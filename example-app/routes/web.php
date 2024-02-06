7bb266941a1c76c3269787be0c783a0bb631e3e6
Kitikawin Hompan <152171870+65160319Kitikawin@users.noreply.github.com>
2024-02-06 18:09:58 +0700

    fix

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\MyAuth;
 use App\Http\Controllers\C_titles;
+use App\Http\Controllers\MyAuth;
 use App\Http\Controllers\MyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -25,7 +25,7 @@ Route::post('/login', [MyAuth::class, 'login_process']);
 Route::post('/register', [MyAuth::class, 'register_process']);
 
 Route::resource('titles', C_titles::class)->middleware('auth');
-Route::middleware('auth')->group(function(){ //เหมือนอันบนแต่เป็นแบบจัด Group
+Route::middleware('auth')->group(function(){
     // auth first
 });
 Route::get('/my-controller', [MyController::class, 'index']);
